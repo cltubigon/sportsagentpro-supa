bbc60ea5b4b1f0cdb92ab895e35c9cb11c7fbb8f
cltubigon <60680621+cltubigon@users.noreply.github.com>
2023-04-15 16:26:39 +0800

    Optimized Quick Access and before installation of @loadable/component

--- client/src/components/ContentArea/HeroSection/ExploreTeams.js
+++ client/src/components/ContentArea/HeroSection/ExploreTeams.js
@@ -2,41 +2,64 @@ import { Box, Flex, Heading, SimpleGrid, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const ExploreTeams = () => {
+  const flexLogoStyle = {
+    alignItems: "center",
+    color: "gray.200",
+    justifyContent: "center",
+    w: "50px",
+    h: "50px",
+    fontSize: "2xl",
+    bg: "gray.500",
+    borderRadius: "100px"
+  }
+  const teamNameStyle = {
+    alignItems: "center",
+    ml: "15px",
+    fontSize: "md",
+    lineHeight: "1.4em",
+  }
+  const teamContainerStyle = {
+    p: "30px",
+    border: "1px solid #cdcdcd",
+    borderRadius: "8px",
+    minHeight: "62px",
+    alignItems: "flex-center",
+  }
   return (
       <Box>
       <Heading as={"h3"} size={"md"} letterSpacing={"1px"} mb={"12px"}>EXPLORE TEAMS</Heading>
       <SimpleGrid minChildWidth={"285px"} spacing='var(--chakra-space-4)'>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
-        <Flex p={"30px"} border={"1px solid #cdcdcd"} borderRadius={"8px"} minHeight={"62px"} alignItems={"flex-center"}>
-          <Flex alignItems={"center"} color={"gray.200"} justifyContent={"center"} w={"50px"} h={"50px"} fontSize={"2xl"} bg={"gray.500"} borderRadius={"100px"} >L</Flex>
-          <Flex  alignItems={"center"} ml={"15px"} fontSize={"md"} lineHeight={"1.4em"} >Oregon Ducks</Flex>
+        <Flex sx={teamContainerStyle}>
+          <Flex sx={flexLogoStyle}>L</Flex>
+          <Flex sx={teamNameStyle} >Oregon Ducks</Flex>
         </Flex>
       </SimpleGrid>
     </Box>
--- client/src/components/ContentArea/HeroSection/FeaturedServices.js
+++ client/src/components/ContentArea/HeroSection/FeaturedServices.js
@@ -2,23 +2,31 @@ import { Box, Heading, SimpleGrid, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const FeaturedServices = () => {
+  const stackStyle = {
+    p: "30px",
+    bg: 'blue.800',
+    borderRadius: "8px",
+    minHeight: "160px",
+    alignItems: "flex-start",
+    justifyContent: "flex-end"
+  }
   return (
     <Box>
       <Heading as={"h3"} size={"md"} letterSpacing={"1px"} mb={"12px"}>FEATURED SERVICES</Heading>
       <SimpleGrid color={"white"} minChildWidth={"290px"} spacing='var(--chakra-space-4)'>
-        <Stack p={"30px"} bg='blue.800' borderRadius={"8px"} minHeight={"160px"} alignItems={"flex-start"} justifyContent={"flex-end"}>
+        <Stack sx={stackStyle}>
           <Text fontSize={"xl"} lineHeight={"1.2em"}>Birthday shoutouts</Text>
           <Text fontSize={"sm"}>Unique and memorable girts</Text>
         </Stack>
-        <Stack p={"30px"} bg='blue.800' borderRadius={"8px"} minHeight={"160px"} alignItems={"flex-start"} justifyContent={"flex-end"}>
+        <Stack sx={stackStyle}>
           <Text fontSize={"xl"} lineHeight={"1.2em"}>Birthday shoutouts</Text>
           <Text fontSize={"sm"}>Unique and memorable girts</Text>
         </Stack>
-        <Stack p={"30px"} bg='blue.800' borderRadius={"8px"} minHeight={"160px"} alignItems={"flex-start"} justifyContent={"flex-end"}>
+        <Stack sx={stackStyle}>
           <Text fontSize={"xl"} lineHeight={"1.2em"}>Birthday shoutouts</Text>
           <Text fontSize={"sm"}>Unique and memorable girts</Text>
         </Stack>
-        <Stack p={"30px"} bg='blue.800' borderRadius={"8px"} minHeight={"160px"} alignItems={"flex-start"} justifyContent={"flex-end"}>
+        <Stack sx={stackStyle}>
           <Text fontSize={"xl"} lineHeight={"1.2em"}>Birthday shoutouts</Text>
           <Text fontSize={"sm"}>Unique and memorable girts</Text>
         </Stack>
