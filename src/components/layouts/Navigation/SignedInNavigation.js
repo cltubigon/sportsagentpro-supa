19cf5709316e64c30ce133d17201c1d5fdf736a4
cltubigon <60680621+cltubigon@users.noreply.github.com>
2023-10-22 23:39:47 +0800

    show to william

--- src/components/layouts/Navigation/SignedInNavigation.js
+++ src/components/layouts/Navigation/SignedInNavigation.js
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react"
+import { Box, Button, Flex } from "@chakra-ui/react"
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { FaFileContract } from "react-icons/fa"
